1df41275206b71e0ce4155765faa0bb7d88aec18
Mikhail Fandyushin <m.fandyushin@gmail.com>
2017-11-21 18:55:45 +0300

    new .emacs.d

--- init.el
+++ init.el
@@ -1,13 +1,21 @@
+;;
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'load-path (concat user-emacs-directory "lisp"))
 
-(require 'init-elpa)
-(require 'init-packages)
-(require 'init-ui)
-(require 'init-cpp)
+(require 'init-core)
+(require 'init-extensions)
+(require 'init-golang)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (exec-path-from-shell use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
